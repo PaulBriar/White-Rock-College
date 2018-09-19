29213e5c082c20035f9d32a494c3c67863c6fd08
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-09-19 14:12:58 -0300

    Added archive.php and finished single.php

--- wp-content/themes/collegetheme/functions.php
+++ wp-content/themes/collegetheme/functions.php
@@ -11,6 +11,9 @@
 
   function college_features() {
     add_theme_support('title-tag');
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocation1', 'Footer Menu Location 1');
+    register_nav_menu('footerLocation2', 'Footer Menu Location 2');
   }
 
   add_action('after_setup_theme', 'college_features');
