6bdf67fb5beffdbd27aec1ac666985b64a58aa67
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-09-26 14:12:31 -0300

    Setup Program Post Type

--- wp-content/mu-plugins/college-post-types.php
+++ wp-content/mu-plugins/college-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function college_post_types() {
+  //Event Post Type
   register_post_type('events', array(
     'supports' => array(
       'title', 'editor', 'excerpt',
@@ -17,6 +18,26 @@ function college_post_types() {
       'edit_item' => 'Edit Event',
       'all_items' => 'All Events',
       'singular_name' => 'event',
+    )
+  ));
+
+  //Program Post Type
+  register_post_type('program', array(
+    'supports' => array(
+      'title', 'editor',
+    ),
+    'rewrite' => array(
+      'slug' => 'programs'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'menu_icon' => 'dashicons-awards',
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'program',
     )
   ));
 }
--- wp-content/themes/collegetheme/functions.php
+++ wp-content/themes/collegetheme/functions.php
@@ -7,13 +7,37 @@
     wp_enqueue_style('college_main_styles', get_stylesheet_uri());
   };
 
-  add_action('wp_enqueue_scripts','white_rock_college_files');
+  add_action( 'wp_enqueue_scripts','white_rock_college_files' );
 
   function college_features() {
-    add_theme_support('title-tag');
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocation1', 'Footer Menu Location 1');
-    register_nav_menu('footerLocation2', 'Footer Menu Location 2');
+    add_theme_support( 'title-tag' );
+    register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
+    register_nav_menu( 'footerLocation1', 'Footer Menu Location 1' );
+    register_nav_menu( 'footerLocation2', 'Footer Menu Location 2' );
   }
 
-  add_action('after_setup_theme', 'college_features');
+  add_action( 'after_setup_theme', 'college_features' );
+
+  function college_adjust_queries( $query ) {
+    $today = date('Ymd');
+    if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query() ) {
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+      $query->set('posts_per_page', -1);
+    }
+    if ( !is_admin() AND is_post_type_archive('events') AND $query->is_main_query() ) {
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric',
+        )
+      ));
+    }
+  }
+
+  add_action( 'pre_get_posts', 'college_adjust_queries' );
